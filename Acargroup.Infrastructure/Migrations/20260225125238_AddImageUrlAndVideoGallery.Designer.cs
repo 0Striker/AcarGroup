ab601664f3e2ef10d254ed8a940e4bd028b225e1
Asım BAKİ <62571697+0Striker@users.noreply.github.com>
2026-02-25 15:52:47 +0300

    db: Add migration for ImageUrl, VideoUrl, GalleryImageUrls

--- Acargroup.Infrastructure/Migrations/20260225125238_AddImageUrlAndVideoGallery.Designer.cs
+++ Acargroup.Infrastructure/Migrations/20260225125238_AddImageUrlAndVideoGallery.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Acargroup.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Acargroup.Infrastructure.Migrations
 {
     [DbContext(typeof(AcargroupDbContext))]
-    partial class AcargroupDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225125238_AddImageUrlAndVideoGallery")]
+    partial class AddImageUrlAndVideoGallery
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Acargroup.Infrastructure/Migrations/20260225125238_AddImageUrlAndVideoGallery.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acargroup.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlAndVideoGallery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Histories",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GalleryImageUrls",
+                table: "CompanyInfos",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VideoUrl",
+                table: "CompanyInfos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Histories");
+
+            migrationBuilder.DropColumn(
+                name: "GalleryImageUrls",
+                table: "CompanyInfos");
+
+            migrationBuilder.DropColumn(
+                name: "VideoUrl",
+                table: "CompanyInfos");
+        }
+    }
+}
